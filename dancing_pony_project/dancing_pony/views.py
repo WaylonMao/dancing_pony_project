e5b367902edbbc5cecce86b2a82b06601258d785
WaylonMao <tomcat_mao@hotmail.com>
2023-08-01 14:30:47 -0600

    Finish Dish CURD with auth

--- dancing_pony_project/dancing_pony/views.py
+++ dancing_pony_project/dancing_pony/views.py
@@ -4,13 +4,16 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import Dish
 from .serializers import DishSerializer
+from rest_framework.permissions import IsAuthenticated
 
 
 class DishListCreateView(generics.ListCreateAPIView):
     queryset = Dish.objects.all()
     serializer_class = DishSerializer
+    permission_classes = [IsAuthenticated]
 
 
 class DishRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Dish.objects.all()
     serializer_class = DishSerializer
+    permission_classes = [IsAuthenticated]
