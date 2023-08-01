32210a0706ff636a9ee456aaba2eb1af8702333d
WaylonMao <tomcat_mao@hotmail.com>
2023-08-01 13:11:44 -0600

    Implement a custom user model

--- dancing_pony_project/dancing_pony/models.py
+++ dancing_pony_project/dancing_pony/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+from django.contrib.auth.models import AbstractUser
+
+
+class CustomUser(AbstractUser):
+    nickname = models.CharField(max_length=255, unique=True)
