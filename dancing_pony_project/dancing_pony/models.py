ffdb23606f9ecacf233f87494819c69e72656f55
WaylonMao <tomcat_mao@hotmail.com>
2023-08-01 13:37:56 -0600

    Implement a dish model

--- dancing_pony_project/dancing_pony/models.py
+++ dancing_pony_project/dancing_pony/models.py
@@ -7,3 +7,13 @@ from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
     nickname = models.CharField(max_length=255, unique=True)
+
+
+class Dish(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField(unique=True)
+    image = models.ImageField(upload_to='dishes/')
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+
+    def __str__(self):
+        return self.name
